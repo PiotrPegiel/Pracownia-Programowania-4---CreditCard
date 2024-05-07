32e82fc9448249e627e3b6a59ce55cb56c7e9383
Piotr Pęgiel <p.a.pegiel@gmail.com>
2024-05-07 02:03:06 +0200

    SQL databese implementation and unit tests

--- src/main/java/pl/krakow/uek/piotrpegiel/ecommerce/catalog/HashMapProductStorage.java
+++ src/main/java/pl/krakow/uek/piotrpegiel/ecommerce/catalog/HashMapProductStorage.java
@@ -1,20 +1,29 @@
 package pl.krakow.uek.piotrpegiel.ecommerce.catalog;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class HashMapProductStorage implements ProductStorage {
-    @Override
-    public List<Product> allProducts() {
-        return null;
+    HashMap<String, Product> productHashMap;
+
+    public HashMapProductStorage() {
+        this.productHashMap = new HashMap<>();
     }
 
     @Override
-    public void add(Product newProduct) {
+    public List<Product> allProducts() {
+        return productHashMap.values().stream().toList();
+    }
 
+    @Override
+    public void add(Product product) {
+        productHashMap.put(product.getId(), product);
     }
 
     @Override
     public Product getProductBy(String id) {
-        return null;
+        return productHashMap.get(id);
     }
+
+
 }
